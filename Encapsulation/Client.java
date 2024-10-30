2a1c088f66b2e85ad89bd8300b49983dbda1a68d
rohitrathore2 <rohitrathore33238@gmail.com>
2024-10-30 12:11:17 +0530

    Encapsulation concept of OOPs in java

--- Encapsulation/Client.java
+++ Encapsulation/Client.java
@@ -1,5 +1,9 @@
 package OOPs.Encapsulation;
-
+    /*
+        following are the ways to achieve Encapsulation :-
+            1. declare all attributes(variable) and behaviour(methods) as private
+            2. provide setter and getter method to modify and view the values of attributes.
+     */
 public class Client {
     public static void main(String[] args) {
         Employee emp = new Employee();
